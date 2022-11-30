6c8b1319c69585050affd2c55bfa4f6ec7ef077e
feijodelima86 <rafael.feijo.bio.uerj@gmail.com>
2022-11-30 15:03:33 -0700

    kEEPIN ON

--- 1_code/2_RFL_Presentation_Figures/12_GLM_GLMM_First_Try.R
+++ 1_code/2_RFL_Presentation_Figures/12_GLM_GLMM_First_Try.R
@@ -8,56 +8,71 @@ library("ggplot2"); theme_set(theme_bw())
 library("tidyverse")
 
 alldata <- data.frame(read_csv("2_incremental/20220420_STANDING_CROP.csv"))
-alldata <- read_csv("2_incremental/20220420_STANDING_CROP_Rafa_Interpolation_Dont_Use.csv")
+#alldata <- read_csv("2_incremental/20220420_STANDING_CROP_Rafa_Interpolation_Dont_Use.csv")
 
 alldata$SAMPLING_DATE<-as.Date(alldata$SAMPLING_DATE, format = "%m/%d/%Y")
 
 COMPARTMENTS <- alldata[which(alldata$SAMPLE_DESCRIPTOR == "EPIL"| alldata$SAMPLE_DESCRIPTOR == "EPIP" | alldata$SAMPLE_DESCRIPTOR == "FILA"),]
 #COMPARTMENTS <- COMPARTMENTS[-217,]
 
-COMPARTMENTS <- alldata[which(alldata$SAMPLE_DESCRIPTOR == "EPIP"),]
+COMPARTMENTS <- alldata[which(alldata$SAMPLE_DESCRIPTOR == "EPIL"),]
+
+#COMPARTMENTS <- COMPARTMENTS[which(COMPARTMENTS$SITE != "WS"),]
 
 COMPARTMENTS$SITE_DISTANCE<-c(WS=0,DL=44.9,GR=64.82,GC=89.22,BG=144.32,BN=167.82)[COMPARTMENTS$SITE]
+
+#COMPARTMENTS<-COMPARTMENTS[-1,]#<- removed outlier*
+
+
+
+
+colnames(COMPARTMENTS)<-"ROWNUM"
+COMPARTMENTS <- COMPARTMENTS[COMPARTMENTS$NA != "1"]
 
 ####Big 5+Fe+Se####
 
 #+39
 
-BIG5.LABELS<-as.data.frame(na.omit(COMPARTMENTS[,c(c(3,4,6,89),c(13,19,22,25,35,40,49)+39)]))
+BIG5.LABELS<-as.data.frame(na.omit(COMPARTMENTS[,c(c(3,4,6,89),c(13,19,22,25,35,40,49))]))
 BIG5.LABELS$SITE<-factor(BIG5.LABELS$SITE)
 #BIG5.LABELS<-BIG5.LABELS[order(BIG5.LABELS$SAMPLE_DESCRIPTOR, decreasing = F), ]
-BIG5.DF<-as.data.frame(BIG5.LABELS[,c(5:ncol(BIG5.LABELS))])
-names(BIG5.DF)<- c("As","Cd","Cu","Fe","Pb","Se","Zn")
-names(BIG5.LABELS)
+names(BIG5.LABELS)[c(5:11)]<- c("As","Cd","Cu","Fe","Pb","Se","Zn")
+
 BIG5.GLM<-BIG5.LABELS
 
 ####Cd####
 
-mix.int <- glm(log(As.1+1) ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
-               family=gaussian)
+mix.int <- glm(As ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
+               family=gaussian(link=log))
 
 summary(mix.int)
 
 
 pframe <- with(BIG5.GLM,
-               expand.grid(SAMPLING_DATE=seq(min(SAMPLING_DATE),max(SAMPLING_DATE),length=51),
+               expand.grid(SAMPLING_DATE=seq(min(SAMPLING_DATE),max(SAMPLING_DATE),length=2),
                            SITE=levels(SITE)))
 
 levels(BIG5.GLM$SITE)
 
-BIG5.GLM$SITE <- factor(BIG5.GLM$SITE, levels = c("WS","DL","GR","GC","BG","BN"), ordered = TRUE)
 
 ## add predicted values (on response scale) to prediction frame
 
-pframe$As.1 <- predict(mix.int,newdata=pframe,type="response")
+pframe$As <- predict.glm(mix.int,newdata=pframe,type="response")
 
-plot2<- ggplot(BIG5.GLM, aes(SAMPLING_DATE, As.1, col = SITE))+
+BIG5.GLM$SITE <- factor(BIG5.GLM$SITE, levels = c("WS","DL","GR","GC","BG","BN"), ordered = F)
+
+plot2<- ggplot(BIG5.GLM, aes(SAMPLING_DATE, log10(As), col = SITE))+
           geom_point() +
-#          scale_y_continuous(trans='log10')+
-          geom_smooth(method = "glm", se = TRUE,
-          method.args = list(family = "gaussian"), linetype = "dashed")+
-          geom_line(data=pframe)  ## use prediction data here
-#dev.new()
+          geom_line(data=pframe)+
+#          coord_trans(x="log10") 
+#         geom_smooth(method = "glm", se = TRUE,
+#          method.args = list(family = "gaussian"), linetype = "dashed")
+#ylim(-1.5, max(BIG5.GLM$As))+
+#          
+
+## use prediction data here
+dev.new()
 
 plot2 + facet_grid( ~ SITE)
+
 
--- 1_code/2_RFL_Presentation_Figures/13_GLM_GLMM_BIG5_PLUS_JUST_MODELS.R
+++ 1_code/2_RFL_Presentation_Figures/13_GLM_GLMM_BIG5_PLUS_JUST_MODELS.R
@@ -8,67 +8,74 @@ library("ggplot2"); theme_set(theme_bw())
 library("tidyverse")
 
 alldata <- data.frame(read_csv("2_incremental/20220420_STANDING_CROP.csv"))
-alldata <- read_csv("2_incremental/20220420_STANDING_CROP_Rafa_Interpolation_Dont_Use.csv")
+#alldata <- read_csv("2_incremental/20220420_STANDING_CROP_Rafa_Interpolation_Dont_Use.csv")
 
 alldata$SAMPLING_DATE<-as.Date(alldata$SAMPLING_DATE, format = "%m/%d/%Y")
 
 EPIL <- alldata[which(alldata$SAMPLE_DESCRIPTOR == "EPIL"),]
 
+EPIL <- alldata[which(EPIL$SITE != "WS"),]
+
+?which
+
 #COMPARTMENTS <- COMPARTMENTS[-217,]
 
 EPIL$SITE_DISTANCE<-c(WS=0,DL=44.9,GR=64.82,GC=89.22,BG=144.32,BN=167.82)[EPIL$SITE]
 
-BIG5.LABELS<-as.data.frame(na.omit(EPIL[,c(c(3,4,6,89),c(13,19,22,25,35,40,49)+39)]))
+#+39
+
+BIG5.LABELS<-as.data.frame(na.omit(EPIL[,c(c(3,4,6,89),c(13,19,22,25,35,40,49+39))]))
 BIG5.LABELS$SITE<-factor(BIG5.LABELS$SITE)
-colnames(BIG5.LABELS[5:11])<-c("As","Cd","Cu","Fe","Pb","Se","Zn")
+names(BIG5.LABELS)[c(5:11)]<- c("As","Cd","Cu","Fe","Pb","Se","Zn")
 BIG5.GLM<-BIG5.LABELS
-BIG5.DF[,c(7)]
+
+
 ####As####
 
-mix.int <- glm(log(As.1*1000) ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
-               family=gaussian(link="log"))
+mix.int <- glm(As ~ SAMPLING_DATE + SITE, data = BIG5.GLM, 
+               family=gaussian)
 
 summary(mix.int)
 
 ####Cd####
 
-mix.int <- glm(log(Cd.1*1000000) ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
-               family=gaussian(link="log"))
+mix.int <- glm(Cd.1 ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
+               family=gaussian)
 
 summary(mix.int)
 
 ####Cu####
 
-mix.int <- glm(log(Cu.1+1) ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
-               family=gaussian(link="log"))
+mix.int <- glm(Cu.1 ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
+               family=gaussian)
 
 summary(mix.int)
 
 ####Fe####
 
-mix.int <- glm(log(Fer.1.1+1) ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
-               family=gaussian(link="log"))
+mix.int <- glm(Fer.1.1 ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
+               family=gaussian)
 
 summary(mix.int)
 
 ####Pb####
 
-mix.int <- glm(log(Pb.1+1) ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
-               family=gaussian(link="log"))
+mix.int <- glm(Pb.1 ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
+               family=gaussian)
 
 summary(mix.int)
 
 ####Se####
 
-mix.int <- glm(log(Se.1+1) ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
-               family=gaussian(link="log"))
+mix.int <- glm(Se.1 ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
+               family=gaussian)
 
 summary(mix.int)
 
 ####Zn####
 
-mix.int <- glm(log(Znr.1+1) ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
-               family=gaussian(link="log"))
+mix.int <- glm(Znr.1 ~ SAMPLING_DATE * SITE, data = BIG5.GLM, 
+               family=gaussian)
 
 summary(mix.int)
 
